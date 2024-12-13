61268584bdc8d7ad6a24658cb27c3bf36c8ad7d4
aidansunbury <aidansunbury@gmail.com>
2024-12-13 06:49:53 -0800

    test 2

--- packages/trpc-ui/src/index.ts
+++ packages/trpc-ui/src/index.ts
@@ -1,4 +1,4 @@
-export { renderTrpcPanel } from "./render";
+export { renderTrpcPanel, type Info } from "./render";
 export type { TRPCPanelMeta } from "./meta";
 export { parseRouterWithOptions } from "./parse/parseRouter";
 // export { RootComponent as TrpcPanel } from "./react-app/Root"
