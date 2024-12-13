0e7834bc1ef9cad428129ea9267a9fab6be0d104
aidansunbury <aidansunbury@gmail.com>
2024-12-13 06:32:31 -0800

    this should not fix anything

--- packages/trpc-ui/src/index.ts
+++ packages/trpc-ui/src/index.ts
@@ -1,4 +1,4 @@
 export { renderTrpcPanel } from "./render";
 export type { TRPCPanelMeta } from "./meta";
 export { parseRouterWithOptions } from "./parse/parseRouter";
-export { RootComponent as TrpcPanel } from "./react-app/Root"
+// export { RootComponent as TrpcPanel } from "./react-app/Root"
