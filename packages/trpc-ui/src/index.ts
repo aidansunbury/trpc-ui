aa56f1fa3dd63920a7a87ca2ef4dba1420f21578
aidansunbury <aidansunbury@gmail.com>
2024-12-13 05:11:28 -0800

    Exporting root component for testing hot refresh

--- packages/trpc-ui/src/index.ts
+++ packages/trpc-ui/src/index.ts
@@ -1,3 +1,4 @@
 export { renderTrpcPanel } from "./render";
 export type { TRPCPanelMeta } from "./meta";
 export { parseRouterWithOptions } from "./parse/parseRouter";
+export { RootComponent as TrpcPanel } from "./react-app/Root"
