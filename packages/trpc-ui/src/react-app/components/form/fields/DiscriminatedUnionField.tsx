573a544c766bad55a9a17e8b7f7124500a405f05
AirBorne04 <daniel.freese@gmail.com>
2026-06-02 14:40:18 +0200

    small fix

--- packages/trpc-ui/src/react-app/components/form/fields/DiscriminatedUnionField.tsx
+++ packages/trpc-ui/src/react-app/components/form/fields/DiscriminatedUnionField.tsx
@@ -28,7 +28,8 @@ export function DiscriminatedUnionField({
     name,
     control,
   });
-  function onDiscriminatorChange(value: string) {
+  function onDiscriminatorChange(value: string | undefined) {
+    if (!value) return;
     const newObj = nodeTypecast.discriminatedUnionChildrenMap[value];
     if (!newObj) return;
     const newDefaultValues = {
